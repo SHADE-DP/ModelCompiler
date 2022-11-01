680cf4bab75c3ac355226a85a79b7ac39e50d26c
Xiao Qi <xiaoqi.loh@digipen.edu>
2022-11-01 13:49:45 +0800

    Removed solution, only project

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,14 @@
 IncludeDir = {}
-IncludeDir["assimp"] = "%{wks.location}\\Dependencies\\assimp"
+IncludeDir["assimp"] = "%{wks.location}\\Dependencies\\assimp" 
 
-workspace "ModelCompileLibrary"
-  architecture "x64"
-  startproject "Assimp Compile Application"
+project "ModelCompileLibrary"
+  kind "ConsoleApp"
+  language "C++"
+  cppdialect "C++20"
+  targetdir (outputdir)
+  objdir    (interdir)
+  systemversion "latest"
+  
   
   configurations
   {
@@ -11,13 +16,67 @@ workspace "ModelCompileLibrary"
     "Release",
     "Publish"
   }
-  
-  flags
+
+  files  
   {
-	  "MultiProcessorCompile"
+    "%{prj.location}/src/**.h",
+    "%{prj.location}/src/**.cpp",
+  }
+
+  externalincludedirs
+  {
+	  "%{IncludeDir.assimp}\\include"
   }
   
-  outputdir = "%{wks.location}/bin/%{cfg.buildcfg}"
-  interdir = "%{wks.location}/bin_int"
+  includedirs
+  {
+    "%{prj.location}/src",
+  }
   
-  include "ModelCompileLibrary"
+  externalwarnings "Off"
+  
+  libdirs
+  {
+    "%{IncludeDir.assimp}/lib/Debug",
+    "%{IncludeDir.assimp}/lib/Release"
+  }
+
+  flags
+  {
+  	"MultiProcessorCompile"
+  }
+
+  filter "configurations:Debug"
+    postbuildcommands 
+    {
+      "xcopy  /r /y /q \"%{IncludeDir.assimp}\\bin\\Debug\\assimp-vc142-mtd.dll\" \"$(OutDir)\""
+    }
+    
+  filter "configurations:Release"
+    postbuildcommands 
+    {
+      "xcopy  /r /y /q \"%{IncludeDir.assimp}\\bin\\Release\\assimp-vc142-mt.dll\" \"$(OutDir)\""
+    }
+
+  filter "configurations:Publish"
+    postbuildcommands 
+    {
+      "xcopy  /r /y /q \"%{IncludeDir.assimp}\\bin\\Release\\assimp-vc142-mt.dll\" \"$(OutDir)\""
+    }
+
+  warnings 'Extra'
+  
+  filter "configurations:Debug"
+    symbols "On"
+    defines {"_DEBUG"}
+    links{"assimp-vc142-mtd.lib"}
+  
+  filter "configurations:Release"
+    optimize "On"
+    defines{"_RELEASE"}
+    links{"assimp-vc142-mt.lib"}
+  
+  filter "configurations:Publish"
+    optimize "On"
+    defines{"_RELEASE, _PUBLISH"}
+    links{"assimp-vc142-mt.lib"}
